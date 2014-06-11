62d8df9c2c470aa28e15bea9488f7eeb8a68092e
Alexander Dajani <xelad1@gmail.com>
2014-06-10 17:33:24 -0700

    Fixed incorrect assignment operators.
    This were misused.  Was receiving "Maximum call stack size exceeded error" due to recursive loop never reaching its base case.

--- Euclid's Algorithm.js
+++ Euclid's Algorithm.js
@@ -6,8 +6,8 @@ function Euclid(num1, num2) {
 
 	if(num1 % num2 !== 0) {
 		var remainder = (num1 % num2);
-		num1 === num2;
-		num2 === remainder;
+		num1 = num2;
+		num2 = remainder;
 
 		return Euclid(num1, num2);
 	}
